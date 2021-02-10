ce2ce341732888016beb2e5fd65068cbab7cf4ba
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-02-10 00:48:09 -0600

    cleanup

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -4,7 +4,6 @@ class Resume extends Component {
   render() {
 
     if(this.props.data){
-      var skillmessage = this.props.data.skillmessage;
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info unset-margin-bottom">{education.degree} <span>&bull;</span><em className="date"> {education.graduated }</em></p>
@@ -64,8 +63,6 @@ class Resume extends Component {
 
          <div className="nine columns main-col">
 
-            {/* <p>{skillmessage}
-            </p> */}
 
 				<div className="bars">
 				   <ul className="skills">
